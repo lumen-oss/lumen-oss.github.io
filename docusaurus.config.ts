5c85752aa4d32ad6b09169a1f3a6eb772dd36332
Vhyrro <vhyrro@gmail.com>
2024-12-21 18:21:38 +0100

    feat: initial commit
    This commit sets up the basics of a docusaurus site and changes a few
    words to use `Rocks` instead of `Docasaurus`.

---
+++ docusaurus.config.ts
@@ -0,0 +1,102 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Rocks',
+  tagline: 'Bringing Lua to the Modern Age',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://rocks.dev',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'rocks', // Usually your GitHub org/user name.
+  projectName: 'rocks', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Rocks',
+      logo: {
+        alt: 'Rocks Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {
+          href: 'https://github.com/nvim-neorocks/rocks',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/nvim-neorocks/rocks',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} The Rocks Team. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ todo.md
@@ -0,0 +1,13 @@
+# TODOs
+
+## Diataxis
+
+Follow diataxis framework:
+Create tutorials, guides etc.
+
+## Subdomains?
+
+Patching the documentation to exist under `/rust-doc` is very bloody difficult
+but we have to figure it out (mostly because the links aren't relative and
+backflip whenever it feels like it). We could make a subdomain but that'll end
+up being complicated if we use github pages.
